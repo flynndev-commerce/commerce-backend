32a6991bcc10db9f90251856aec192800e3fba5e
Flynn Park <wphestiraid@gmail.com>
2026-01-06 09:24:56 +0900

    test(product): 상품 수정 API 테스트에 판매자 권한 헤더 추가

--- tests/v1/products/test_product_detail.py
+++ tests/v1/products/test_product_detail.py
@@ -11,6 +11,7 @@ from tests.v1.products.helpers import (
     TEST_PRODUCT_NAME_UPDATED,
     TEST_PRODUCT_PRICE_UPDATED,
     create_test_product,
+    create_test_seller,
 )
 
 
@@ -46,9 +47,11 @@ class TestProductUpdate:
     def test_update_product_success(self, test_app: FastAPI, client: TestClient) -> None:
         """상품 수정 성공 테스트"""
         product = create_test_product(test_app, client)
+        headers = create_test_seller(test_app, client)
 
         response = client.patch(
             test_app.url_path_for(RouteName.PRODUCTS_UPDATE, product_id=product.id),
+            headers=headers,
             json={
                 "name": TEST_PRODUCT_NAME_UPDATED,
                 "price": TEST_PRODUCT_PRICE_UPDATED,
@@ -65,8 +68,11 @@ class TestProductUpdate:
 
     def test_update_product_not_found(self, test_app: FastAPI, client: TestClient) -> None:
         """존재하지 않는 상품 수정 실패 테스트"""
+        headers = create_test_seller(test_app, client)
+
         response = client.patch(
             test_app.url_path_for(RouteName.PRODUCTS_UPDATE, product_id=TEST_PRODUCT_ID_NONEXISTENT),
+            headers=headers,
             json={"name": TEST_PRODUCT_NAME_UPDATED},
         )
 
